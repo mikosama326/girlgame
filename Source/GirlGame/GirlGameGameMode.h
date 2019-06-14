487d077a6a75c7d9746502740403be8522ec8e16
Srija <u1209446@utah.edu>
2019-06-13 21:15:26 -0600

    Adding the project files, with basic AI

---
+++ Source/GirlGame/GirlGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GirlGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGirlGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGirlGameGameMode();
+};
+
+
+
